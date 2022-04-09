590f1f5f899f7d35b6cff8076c4d7b51816b2dfe
pochun-chiu <pochun.chiu@instill.tech>
2022-04-09 13:32:13 +0800

    refactor: clean up

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,15 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import App from './App'
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
-
 import { render } from "../../packages/huyu-core/src/reconcile";
 import { createElement } from "../../packages/huyu-core/src/create-element";
 
