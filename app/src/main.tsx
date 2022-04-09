70d85d151f391da93a29397d1a56920b07ab6a96
pochun-chiu <pochun.chiu@instill.tech>
2022-04-09 13:11:04 +0800

    feat: add init vite website with huyu

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+import { render } from "../../packages/huyu-core/src/reconcile";
+import { createElement } from "../../packages/huyu-core/src/create-element";
+
+const vNode = createElement("div", {}, []);
+render(vNode, document.getElementById("root"))
