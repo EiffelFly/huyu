45f8b8ead375aba825e10f11a03a72cf86973be7
pochun-chiu <pochun.chiu@instill.tech>
2022-04-09 13:12:07 +0800

    feat: add init render method

--- packages/huyu-core/src/reconcile.ts
+++ packages/huyu-core/src/reconcile.ts
@@ -1,9 +1,20 @@
-export const enum LANE {
-  UPDATE = 1 << 1,
-  INSERT = 1 << 2,
-  REMOVE = 1 << 3,
-  SVG = 1 << 4,
-  DIRTY = 1 << 5,
-  HEAD = 1 << 6,
-  NOWORK = 1 << 7,
-}
+import { SVG_ELEMENT, TEXT_ELEMENT } from "./constant";
+import { VNode } from "./type";
+
+export const render = (vNode: VNode, ownerDom: Element | null) => {
+  let dom: Text | Element;
+  let nodeType = vNode.type;
+  if (nodeType === TEXT_ELEMENT) {
+    dom = document.createTextNode("");
+  } else if (nodeType === SVG_ELEMENT) {
+    dom = document.createElementNS("http://www.w3.org/2000/svg", nodeType);
+  } else {
+    dom = document.createElement(nodeType);
+  }
+
+  if (!ownerDom) {
+    return dom;
+  } else {
+    return ownerDom.appendChild(dom);
+  }
+};
