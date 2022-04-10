b33d3c847c1eb25986ae0f5d9a5a16e0ed40b020
pochun-chiu <pochun.chiu@instill.tech>
2022-04-10 15:21:38 +0800

    feat: update app main

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,26 +1,14 @@
-import { render } from "@huyu/core";
-import { createElement } from "@huyu/core";
+import { render, createElement, h } from "@huyu/core";
 
-const hi = createElement("div", {}, ["hi"]);
-const iAm = createElement("div", {}, ["I am"]);
+/* The old way to generate element and related dom object */
 
-const headerContainer = createElement("h1", {}, [iAm, hi]);
+// const hi = createElement("div", {}, "hi");
+// const iAm = createElement("div", {}, "I am");
+// const headerContainer = createElement("h1", {}, iAm, hi);
+// render(headerContainer, document.getElementById("root"));
 
-const span1 = createElement("span", {}, ["span1"]);
-const span2 = createElement("span", {}, ["span2"]);
+/* the jsx way */
 
-const spanContainer = createElement("div", {}, [span1, span2]);
+const test = <div>test</div>;
 
-const li1 = createElement("li", {}, ["li1"]);
-const li2 = createElement("li", {}, ["li2"]);
-const li3 = createElement("li", {}, ["li2", span1]);
-
-const ulContainer = createElement("ul", {}, [li1, li2, li3]);
-
-const mainContainer = createElement("div", {}, [
-  headerContainer,
-  spanContainer,
-  ulContainer,
-]);
-
-render(mainContainer, document.getElementById("root"));
+render(test, document.getElementById("root"));
