37c0a777cafdd7d011568444490426410bca0712
pochun-chiu <pochun.chiu@instill.tech>
2022-04-10 10:27:45 +0800

    feat: add method to render children

--- packages/huyu-core/src/reconcile.ts
+++ packages/huyu-core/src/reconcile.ts
@@ -1,20 +1,34 @@
 import { SVG_ELEMENT, TEXT_ELEMENT } from "./constant";
 import { VNode } from "./type";
 
-export const render = (vNode: VNode, ownerDom: Element | null) => {
-  let dom: Text | Element;
+export const render = (vNode: VNode, ownerDom: Element | null | Text) => {
+  let element: Text | Element;
   let nodeType = vNode.type;
+
   if (nodeType === TEXT_ELEMENT) {
-    dom = document.createTextNode("");
+    element = document.createTextNode(
+      (vNode as VNode<{ nodeValue: string }>).props.nodeValue
+    );
   } else if (nodeType === SVG_ELEMENT) {
-    dom = document.createElementNS("http://www.w3.org/2000/svg", nodeType);
+    element = document.createElementNS("http://www.w3.org/2000/svg", nodeType);
   } else {
-    dom = document.createElement(nodeType);
+    element = document.createElement(nodeType);
   }
 
+  // This is not how the react work, in practice we should setting attributes with setAttribute
+  // and using a synthetic event system to connect the events in the DOM with events in the VDOM.
+  // https://itnext.io/creating-our-own-react-from-scratch-82dd6356676d
+
+  // for (const attributes in vNode.props || {}) {
+  //   element[attributes] = vNode.props[attributes];
+  // }
+
+  // recursive append children element
+  (vNode.props.children || []).forEach((child) => render(child, element));
+
   if (!ownerDom) {
-    return dom;
+    return element;
   } else {
-    return ownerDom.appendChild(dom);
+    return ownerDom.appendChild(element);
   }
 };
