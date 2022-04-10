b98a71a02b57c41b97afb2ecbafc9496aebfa85e
pochun-chiu <pochun.chiu@instill.tech>
2022-04-10 10:27:18 +0800

    feat: add various createElement

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,5 +1,26 @@
 import { render } from "@huyu/core";
 import { createElement } from "@huyu/core";
 
-const vNode = createElement("div", {}, []);
-render(vNode, document.getElementById("root"));
+const hi = createElement("div", {}, ["hi"]);
+const iAm = createElement("div", {}, ["I am"]);
+
+const headerContainer = createElement("h1", {}, [iAm, hi]);
+
+const span1 = createElement("span", {}, ["span1"]);
+const span2 = createElement("span", {}, ["span2"]);
+
+const spanContainer = createElement("div", {}, [span1, span2]);
+
+const li1 = createElement("li", {}, ["li1"]);
+const li2 = createElement("li", {}, ["li2"]);
+const li3 = createElement("li", {}, ["li2", span1]);
+
+const ulContainer = createElement("ul", {}, [li1, li2, li3]);
+
+const mainContainer = createElement("div", {}, [
+  headerContainer,
+  spanContainer,
+  ulContainer,
+]);
+
+render(mainContainer, document.getElementById("root"));
