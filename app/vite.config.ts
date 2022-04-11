505e571cec1c0a58801043b1755ee956fd31f961
pochun-chiu <pochun.chiu@instill.tech>
2022-04-11 10:47:08 +0800

    feat: add auto import jsx vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+  esbuild: {
+    jsxFactory: "_jsx",
+    jsxFragment: "_jsxFragment",
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from "@huyu/core";`,
+  },
+});
