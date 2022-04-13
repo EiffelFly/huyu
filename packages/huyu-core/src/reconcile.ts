80785f484a7b326abeea28b4f75f68f223e6bcb4
pochun-chiu <pochun.chiu@instill.tech>
2022-04-13 11:23:20 +0800

    feat: rebuild render function and add createDOM function

--- packages/huyu-core/src/reconcile.ts
+++ packages/huyu-core/src/reconcile.ts
@@ -1,44 +1,32 @@
 import { SVG_ELEMENT, TEXT_ELEMENT } from "./constant";
-import { FC, VNode } from "./type";
+import { createVNode } from "./create-element";
+import { FC, HuyuElement, VDom, VNode } from "./type";
 
-export const render = (vNode: VNode, ownerDom: Element | null | Text) => {
+export const render = (
+  huyuElement: HuyuElement,
+  ownerDom: Element | null | Text
+) => {
+  let vNode = createVNode(huyuElement);
+  let dom = createDOM(vNode, ownerDom);
+};
+
+export const createDOM = (vDom: VDom, ownerDom: Element | null | Text) => {
+  if (Array.isArray(vDom)) {
+    return vDom.map((d) => createDOM(d, ownerDom));
+  }
   let element: Text | Element;
-  let wip: VNode;
 
-  if (typeof vNode.type === "function") {
-    console.log("hi i am function component");
-
-    wip = vNode.type(vNode.props);
-
-  } else if (typeof vNode.type === "object") {
-    console.log("hi i am named component");
-    wip = vNode.type;
-  } else {
-    wip = vNode;
-  }
-
-  let wipType = wip.type as string;
-
-  if (wipType === TEXT_ELEMENT) {
+  if (vDom.type === TEXT_ELEMENT) {
     element = document.createTextNode(
-      (wip as VNode<{ nodeValue: string }>).props.nodeValue
+      (vDom as VNode<{ nodeValue: string }>).props.nodeValue
     );
-  } else if (wipType === SVG_ELEMENT) {
-    element = document.createElementNS("http://www.w3.org/2000/svg", wipType);
+  } else if (vDom.type === SVG_ELEMENT) {
+    element = document.createElementNS("http://www.w3.org/2000/svg", vDom.type);
   } else {
-    element = document.createElement(wipType);
+    element = document.createElement(vDom.type as string);
   }
 
-  // This is not how the react work, in practice we should setting attributes with setAttribute
-  // and using a synthetic event system to connect the events in the DOM with events in the VDOM.
-  // https://itnext.io/creating-our-own-react-from-scratch-82dd6356676d
-
-  // for (const attributes in vNode.props || {}) {
-  //   element[attributes] = vNode.props[attributes];
-  // }
-
-  // recursive append children element
-  (wip.props.children || []).forEach((child) => render(child, element));
+  (vDom.props.children || []).forEach((child) => render(child, element));
 
   if (!ownerDom) {
     return element;
