e72c83f2fa903b239d8cc275176b50c0343d459f
pochun-chiu <pochun.chiu@instill.tech>
2022-04-22 13:41:59 +0800

    refactor: refactor core export name

--- packages/huyu-core/src/index.ts
+++ packages/huyu-core/src/index.ts
@@ -2,4 +2,4 @@ import { createElement, Fragment } from "./create-element";
 import { render } from "./reconcile";
 
 export * from "./constant";
-export { createElement, createElement as h, render, Fragment };
+export { createElement, createElement as _jsx, render, Fragment, Fragment as _jsxFragment };
