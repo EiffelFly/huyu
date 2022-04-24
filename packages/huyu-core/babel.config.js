d98b5d528c98da6058713dae9b365eae00d28d8c
pochun-chiu <pochun.chiu@instill.tech>
2022-04-24 10:52:54 +0800

    fix: fix config option name typo

--- packages/huyu-core/babel.config.js
+++ packages/huyu-core/babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     ["@babel/preset-env", { targets: { node: "current" } }],
     [
       "@babel/preset-typescript",
-      { jsxPragma: "_jsx", pragmaFrag: "_jsxFragment" },
+      { jsxPragma: "_jsx", jsxPragmaFrag: "_jsxFragment" },
     ],
   ],
   plugins: [
